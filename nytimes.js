860734529255daed4322396d2ef857569dcc9435
scoslo5512 <robert.queeney@gmail.com>
2018-03-20 17:45:58 -0700

    added basic js coding

--- nytimes.js
+++ nytimes.js
@@ -1,4 +1,12 @@
 "use script";
 $(document).ready(function(){
 
-});
+
+    $.ajax({
+        url: "https://api.nytimes.com/svc/archive/v1/2016/1.json?api-key=e23ed19aa58241c0a78b6d663b44e23f",
+        method: 'GET',
+    }).then(function(response){
+        console.log(response); 
+    })
+
+    });
