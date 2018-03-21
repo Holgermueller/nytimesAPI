ecffe5c2434480846102d5d8f6b5991b86f5f766
atuffa <tuffajr@gmail.com>
2018-03-20 19:56:24 -0500

    Adding variable to api url

--- nytimes.js
+++ nytimes.js
@@ -1,4 +1,15 @@
 "use script";
 $(document).ready(function(){
 
+
+    var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q"+searchfield+
+     "facet_field=source&begin_date="+beginyear+"0101&end_date="+endyear+"1231&api-key=7ed0a22d92cc4cd9b5fd37efbca49ff9";
+    // varq=obama&facet_field=source&begin_date=20120101&end_date=20121231
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+});
 });
