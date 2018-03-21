fa63f1047217aa9b43c76d9cec20ba15a7a0827d
atuffa <tuffajr@gmail.com>
2018-03-20 20:54:39 -0500

    url modification

--- nytimes.js
+++ nytimes.js
@@ -1,17 +1,24 @@
 "use script";
 $(document).ready(function(){
-
-
-    var queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q"+searchfield+
-     "facet_field=source&begin_date="+beginyear+"0101&end_date="+endyear+"1231&api-key=7ed0a22d92cc4cd9b5fd37efbca49ff9";
-    // varq=obama&facet_field=source&begin_date=20120101&end_date=20121231
+    
+    
+    
 
     $("#search-button").on("click",function(){
+
+        let searchField = $("#search-input").val()
+        let beginYear = $("#end-year").val()
+        let endYear = $("#start-year").val()
+        let dropDown= $("#num-records").val()
+
+        let queryURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchField+"&api-key=7ed0a22d92cc4cd9b5fd37efbca49ff9";
+   
         $.ajax({
-        url: "http://api.nytimes.com/svc/search/v2/articlesearch.json?qdenzelfacet_field=source&begin_date=20010101&end_date=20021231&api-key=7ed0a22d92cc4cd9b5fd37efbca49ff9",
+        url:queryURL,
         method: "GET"
-        }).then(function renderNewsStories(response){
+        }).then(function(response){
           console.log(response);
+          console.log(searchField);
         // const newsTemplate = `<div class="well">
         // <h3> <lable class="label-primary">${count}</lable> ${Title}</h3>
         // <h5>${author}</h5>
