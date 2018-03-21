ab34050cb3456bd3b3c85f27a7b8c1aedd85eac3
Abraham <abeis@msn.com>
2018-03-20 19:53:15 -0500

    added comments and updated function name

--- nytimes.js
+++ nytimes.js
@@ -4,11 +4,16 @@ $(document).ready(function(){
 });
 
 
-
-function displayToPage(){
+/**
+ *  Used to diaply News articles to the page
+ * 
+ * @method renderNewsStories
+ */
+function renderNewsStories(){
     const newsTemplate = `<div class="well">
     <h3> <lable class="label-primary">${count}</lable> ${Title}</h3>
     <h5>${author}</h5>
     </div>`;
 
+    $('.results').append(newsTemplate);
 }
